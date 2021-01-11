aba7e9ecc6fe7b7d6c34d9b6b957d3e65fb2d30e
NaikyDing <Naiky.ding@goldenf.co>
2021-01-11 11:44:40 +0800

    git init

---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input v-model="form.username" type="text" />
+    <input v-model="form.password" type="password" />
+    <button @click="userLogin">LOGIN</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    form: {
+      username: "",
+      password: ""
+    }
+  }),
+
+  methods: {
+    userLogin() {
+      console.log("LOGIN");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
